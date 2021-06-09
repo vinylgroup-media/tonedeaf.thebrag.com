d349c1706b878b325fe92091c428b0c4069c713c
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-09 13:01:01 +1000

    Update functions.php

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -1684,7 +1684,7 @@ function ssm_inject_ads($content)
     render_ad_tag('incontent_1', $count_articles);
     $content_ad_tag = ob_get_contents();
     ob_end_clean();
-    $content = ssm_insert_after_paragraph('<div class="my-2 text-center">' . $content_ad_tag . '</div>', $after_para, $content);
+    $content = ssm_insert_after_paragraph('<div class="my-2 text-center ad-mrec">' . $content_ad_tag . '</div>', $after_para, $content);
 
     return $content;
 }
