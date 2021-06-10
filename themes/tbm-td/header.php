b54b3a138592c3d9dcab2ce59c2e89f627f1b0d2
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-10 16:59:31 +1000

    Update favicons

--- themes/tbm-td/header.php
+++ themes/tbm-td/header.php
@@ -17,6 +17,14 @@ $current_url = home_url(add_query_arg([], $GLOBALS['wp']->request));
   <meta property="fb:pages" content="105156786184223" />
   <meta property="fb:app_id" content="812299355633906" />
 
+  <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+  <link rel="manifest" href="/icons/site.webmanifest">
+  <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#1e81ef">
+  <link rel="shortcut icon" href="/icons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="/icons/browserconfig.xml">
   <meta name="theme-color" content="#1E81EF">
 
   <?php if (is_single()) {
