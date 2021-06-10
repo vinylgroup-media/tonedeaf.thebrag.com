86bb566a391907a0da6effe2e09d8de7c212b253
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-10 12:03:17 +1000

    Update page-festivals.php

--- themes/tbm-td/page-festivals.php
+++ themes/tbm-td/page-festivals.php
@@ -1,42 +1,43 @@
 <?php /* Template Name: Festivals ( List ) */ ?>
 <?php get_header(); ?>
 
-<div class="container artists">
+<section class="container latest p-2">
     <div class="row">
         <h1 class="col-12 archive-title my-3"><?php the_title(); ?></h1>
     </div>
     <div class="row">
-      <div class="col-12">
-        <?php the_content(); ?>
-      </div>
+        <div class="col-12">
+            <?php the_content(); ?>
+        </div>
     </div>
-    <div class="row posts">
+    <div class="d-flex flex-wrap align-items-start mt-2">
         <?php
         global $wpdb;
-        $artists = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}td_artist_urls WHERE url_slug = 'festival' ORDER BY artist_name ASC" );
-        if ( count ( $artists ) > 0 ) :
-            foreach ( $artists as $key => $artist ) :
-            if ( isset( $artist->image_id ) && $artist->image_id > 0 ):
-                $artist_img_src = wp_get_attachment_image_src( $artist->image_id, 'medium_large' );
+        $artists = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}td_artist_urls WHERE url_slug = 'festival' ORDER BY artist_name ASC");
+        if (count($artists) > 0) :
+            foreach ($artists as $key => $artist) :
+                if (isset($artist->image_id) && $artist->image_id > 0) :
+                    $artist_img_src = wp_get_attachment_image_src($artist->image_id, 'medium_large');
         ?>
-        <div class="col-md-4 col-6 mb-5">
-            <a href="/<?php echo $artist->url_slug; ?>/<?php echo $artist->artist_slug; ?>" class="text-dark">
-                <div class="post-thumbnail">
-                    <img src="<?php echo $artist_img_src[0]; ?>" alt="<?php echo $artist->artist_name; ?>" title="<?php echo $artist->artist_name; ?>">
-                </div>
-                <h3 class="artist-name mt-1"><?php echo $artist->artist_name; ?></h3>
-                <p><?php echo $artist->metadesc; ?></p>
-            </a>
-        </div>
+                    <div class="article-wrap col-12 col-md-4">
+                        <article class="my-1 my-md-3">
+                            <div class="mb-4 mx-0 mx-md-3">
+                                <a href="/<?php echo $artist->url_slug; ?>/<?php echo $artist->artist_slug; ?>" class="d-flex flex-row flex-md-column align-items-start">
+                                    <div class="post-thumbnail p-r">
+                                        <img src="<?php echo $artist_img_src[0]; ?>" alt="<?php echo $artist->artist_name; ?>" title="<?php echo $artist->artist_name; ?>">
+                                    </div>
+                                    <div class="pl-2 post-content align-self-start col-auto">
+                                        <h3 class="mb-2"><?php echo $artist->artist_name; ?></h3>
+                                        <p><?php echo $artist->metadesc; ?></p>
+                                    </div>
+                                </a>
+                            </div>
+                        </article>
+                    </div>
         <?php endif;
             endforeach;
         endif;
         ?>
     </div>
-
-    <div class="my-3 pb-2"><?php get_fuse_tag( 'hrec_2' ); ?></div>
-</div>
-
-
-
+</section>
 <?php get_footer();
