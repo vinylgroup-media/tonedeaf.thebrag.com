b0f8e73e2496bb9f772ed3f7e7adf5abdf4ae46a
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-11 23:26:39 +1000

    Update header.php

--- themes/tbm-td/header.php
+++ themes/tbm-td/header.php
@@ -383,7 +383,7 @@ $current_url = home_url(add_query_arg([], $GLOBALS['wp']->request));
 
     <div id="mobile-nav-wrap" class="nav-wrap container flex-column p-r justify-content-between" style="display: none;">
       <div id="search-nav-wrap" class="w-100 d-flex flex-column flex-fill">
-        <div id="top-search-wrap-mobile" style="position: absolute; top: -.25rem; left: 0; z-index: 10; background: transparent; border-radius: .5rem; width: 320px; transition: .25s left linear; border: 1px solid rgba(255,255,255,.15);">
+        <div id="top-search-wrap-mobile" style="position: absolute; top: -.25rem; left: 0; z-index: 10; background: rgba(255,255,255,.75); border-radius: .5rem; width: 320px; transition: .25s left linear; border: 1px solid rgba(255,255,255,.15);">
           <form role="search" method="get" id="searchform" class="searchform d-flex justify-content-center" action="<?php echo esc_url(home_url('/')); ?>" style="">
             <div class="btn btn-close-top-search" style="padding: .25rem; margin: auto .25rem; opacity: .5;">
               <img src="<?php echo ICONS_URL; ?>cross-dark.svg" width="16" height="16" alt="<" style="width: 8px; height: auto;">
