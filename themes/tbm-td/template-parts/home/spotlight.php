c1b20b330d0b2eaa64118b954ad41a0a971f9d30
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-11 14:27:13 +1000

    Update correct file for Spotlight

--- themes/tbm-td/template-parts/home/spotlight.php
+++ themes/tbm-td/template-parts/home/spotlight.php
@@ -1,39 +1,62 @@
-<?php extract($args); ?>
 <section class="container spotlight">
     <div class="m-2">
         <h2 class="text-center text-uppercase p-1 pt-0 mb-0 mx-1 h-spotlight">Spotlight</h2>
-        <div class="spotlight-stories d-flex <?php echo isset($args['pos']) && 'sidebar' == $args['pos'] ? 'flex-column' : 'flex-row'; ?> align-items-start rounded bg-white mt-2">
+        <div class="spotlight-stories d-flex flex-row align-items-start rounded bg-white mt-2">
             <?php
-            while ($spotlight_articles->have_posts()) :
-                $spotlight_articles->the_post();
-                // $categories = get_the_category(get_the_ID());
-                $genres = get_the_terms(get_the_ID(), 'genre');
+            global $wpdb;
+            $spotlight_article_ids = $wpdb->get_results(
+                "SELECT post_id FROM ( 
+                    SELECT post_id FROM `{$wpdb->prefix}tbm_trending`
+                    ORDER BY `created_at` DESC LIMIT 10
+                    ) AS temptable
+                    ORDER BY RAND()
+                    LIMIT 20"
+            );
+            if ($spotlight_article_ids && count($spotlight_article_ids) > 0) :
+                $spotlight_articles_args = [
+                    'post_status' => 'publish',
+                    // 'post_type' => array('post', 'country'),
+                    'ignore_sticky_posts' => 1,
+                    'posts_per_page' => 3,
+                    'orderby'        => 'rand',
+                    'post__in' => wp_list_pluck($spotlight_article_ids, 'post_id'),
+                ];
+                $spotlight_articles = new WP_Query($spotlight_articles_args);
+                if ($spotlight_articles->have_posts()) :
             ?>
-                <a href="<?php the_permalink(); ?>" class="story m-1 m-md-2 pb-0">
-                    <div class="d-flex flex-column flex-md-row align-items-start">
-                        <div class="img-wrap rounded mr-0 mr-md-2">
-                            <?php if ('' !== get_the_post_thumbnail()) :
-                                the_post_thumbnail('thumbnail');
-                            endif; ?>
-                        </div>
-                        <div>
-                            <div class="mb-1 mt-1 mt-md-0 text-uppercase spotlight-story-category">
-                                <?php
-                                if ($genres) :
-                                    foreach ($genres as $genre) :
-                                        echo $genre->name;
-                                        break;
-                                    endforeach; // For Each Genre
-                                endif; // If there are genres for the post 
-                                ?>
-                            </div><!-- Cats -->
-                            <h3 class="h6"><?php the_title(); ?></h3>
-                        </div>
+                    <?php
+                    while ($spotlight_articles->have_posts()) :
+                        $spotlight_articles->the_post();
+                        // $categories = get_the_category(get_the_ID());
+                        $genres = get_the_terms(get_the_ID(), 'genre');
+                    ?>
+                        <a href="<?php the_permalink(); ?>" class="story m-1 m-md-2 pb-0">
+                            <div class="d-flex flex-column flex-md-row align-items-start">
+                                <div class="img-wrap rounded mr-0 mr-md-2">
+                                    <?php if ('' !== get_the_post_thumbnail()) :
+                                        the_post_thumbnail('thumbnail');
+                                    endif; ?>
+                                </div>
+                                <div>
+                                    <div class="mb-1 mt-1 mt-md-0 text-uppercase spotlight-story-category">
+                                        <?php
+                                        if ($genres) :
+                                            foreach ($genres as $genre) :
+                                                echo $genre->name;
+                                                break;
+                                            endforeach; // For Each Genre
+                                        endif; // If there are genres for the post 
+                                        ?>
+                                    </div><!-- Cats -->
+                                    <h3 class="h6"><?php the_title(); ?></h3>
+                                </div>
 
-                    </div>
-                </a>
+                            </div>
+                        </a>
             <?php endwhile;
-            wp_reset_postdata();
+                    wp_reset_postdata();
+                endif; // If there are spotlight articles
+            endif;
             ?>
         </div>
     </div>
