b2fc723abeec236c4a86abf21ba1b4799388a527
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-11 16:39:27 +1000

    Update image

--- themes/tbm-td/page-templates/page-solstice-2021.php
+++ themes/tbm-td/page-templates/page-solstice-2021.php
@@ -2249,7 +2249,7 @@ get_template_part('template-parts/header/solstice');
                             <div class="row align-items-center">
                                 <div class="col-md-4 my-3 align-self-stretch pr-0 pr-md-2">
                                     <div style="position: sticky; top: 60px;">
-                                        <img src="<?php echo get_template_directory_uri(); ?>/images/solstice-2021/Tone-Deaf_STAR_RUBY.jpg">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/images/solstice-2021/TD_STARGIG_600px.jpg">
                                     </div>
                                 </div>
                                 <div class="col-md-8 my-3">
