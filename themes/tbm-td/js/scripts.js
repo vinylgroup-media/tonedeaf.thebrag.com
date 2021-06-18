b5b7076b750416dce330b64fa5d9d1e65a15c7b6
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-18 16:53:22 +1000

    Update BB on mobile scroll

--- themes/tbm-td/js/scripts.js
+++ themes/tbm-td/js/scripts.js
@@ -218,11 +218,14 @@ jQuery(document).ready(function ($) {
       $('#skin').removeClass('fixed');
     } */
 
-    /* var mainTop = $('main').length ? $('main').offset().top : 0;
-    var billboardHeight = $('.ad-billboard').length ? $('.ad-billboard').height() : 0;
-    if (winTop >= mainTop + billboardHeight) {
-      $('.sticky-ad-bottom').show();
-    } else {
+    if ($(window).width() < 768) {
+      var mainTop = $('main').length ? $('main').offset().top : 0;
+      var billboardHeight = $('.ad-billboard-top').length ? $('.ad-billboard-top').height() : 0;
+      if (winTop >= $(window).height() / 2) {
+        $('.ad-billboard-top').addClass('sticky').slideDown();
+      }
+    }
+    /*  else {
       $('.sticky-ad-bottom').hide();
     } */
 
