3ed39e97aa3360d8c77c2f940df07ea20c33693c
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-06-22 15:28:23 +1000

    Update functions.php

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -551,16 +551,6 @@ function register_cpt_photo_gallery()
             case "photoartists":
                 echo get_the_term_list($post->ID, 'artist', '', ', ', '');
                 break;
-            case "genre":
-                $genres = get_the_terms($post->ID, 'genre');
-                if ($genres) {
-                    $output = array();
-                    foreach ($genres as $genre) {
-                        // $output[] = '<a href="' . get_term_link($genre->slug, 'genre') . '">' . $genre->name . '</a>';
-                    }
-                    echo join(', ', $output);
-                }
-                break;
         }
     }
 }
