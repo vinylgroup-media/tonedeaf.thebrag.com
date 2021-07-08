01fd5c4acd5a7d6bcce79874b67b99183725bc30
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-07-08 13:25:13 +1000

    Update functions.php

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -1684,7 +1684,7 @@ function ssm_inject_ads($content)
         return $content;
     endif;
 
-    if (!is_singular('post')) {
+    if (!is_singular('post') || !is_singular('country')) {
         return $content;
     }
 
