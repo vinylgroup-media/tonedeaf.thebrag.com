56cd7894974eca7fcb10b162efd0c008d58c7ecc
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-07-23 09:33:06 +1000

    Update CSS for blockquote

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
   <?php // if (!is_page_template('page-quiz.php')) : 
   ?>
-  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20210617.2" type="text/css" media="all" />
+  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20210723" type="text/css" media="all" />
   <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" /> -->
   <?php // endif; 
   ?>
@@ -117,7 +117,8 @@ position: fixed;
     text-align: center;
     z-index: 4;
     width: 100%;">
-  <?php // render_ad_tag('mob_sticky'); ?>
+  <?php // render_ad_tag('mob_sticky'); 
+  ?>
 </div> -->
 
 </body>
