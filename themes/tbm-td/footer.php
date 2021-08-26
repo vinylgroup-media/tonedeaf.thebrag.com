1c6f20a9edf429ea8bd632fdd8481a787c5552d7
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-08-26 10:48:08 +1000

    Update footer.php

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
   <?php // if (!is_page_template('page-quiz.php')) : 
   ?>
-  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20210723.3" type="text/css" media="all" />
+  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20210826" type="text/css" media="all" />
   <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" /> -->
   <?php // endif; 
   ?>
