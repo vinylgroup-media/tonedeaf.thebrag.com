59249466659758ede66a2701e499500856e842cf
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2021-10-14 22:24:48 +1100

    Add brand lift study code

--- plugins/theme-options/theme-options.php
+++ plugins/theme-options/theme-options.php
@@ -637,3 +637,32 @@ add_action('wp_footer', function () {
     </script>
 <?php
 });
+
+// Brand lift study
+add_action('wp_footer', function () {
+?>
+    <script>
+        {
+            const e = (e, t, n, s) => {
+                    var c, o, r;
+                    e.SMCX = e.SMCX || [], t.getElementById(s) || (o = (c = t.getElementsByTagName(n))[c.length - 1], (r = t.createElement(n)).type = "text/javascript", r.async = !0, r.id = s, r.src = "https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd36CitCEEwoE44pTCPBWttcrfN2mODXNCsr6H61j_2BkMD.js", o.parentNode.insertBefore(r, o))
+                },
+                t = (e, t, n, s) => {
+                    var c, o, r;
+                    e.SMCX = e.SMCX || [], t.getElementById(s) || (o = (c = t.getElementsByTagName(n))[c.length - 1], (r = t.createElement(n)).type = "text/javascript", r.async = !0, r.id = s, r.src = "https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd36CitCEEwoE44pTCPBWtteffxwhXTTNQIUFZGZf1MZH.js", o.parentNode.insertBefore(r, o))
+                };
+            null !== (() => {
+                var e = document.cookie,
+                    t = e.indexOf("; tbm_v=");
+                if (-1 == t) {
+                    if (0 != (t = e.indexOf("tbm_v="))) return null
+                } else {
+                    t += 2;
+                    var n = document.cookie.indexOf(";", t); - 1 == n && (n = e.length)
+                }
+                return decodeURI(e.substring(t + "tbm_v=".length, n))
+            })() ? (console.log("cookie"), e(window, document, "script", "smcx-sdk")) : (console.log("no cookie"), t(window, document, "script", "smcx-sdk"))
+        }
+    </script>
+<?php
+});
