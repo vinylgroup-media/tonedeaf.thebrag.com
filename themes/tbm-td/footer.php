68eaf1fb1732e1377d341747e9f60696cb994203
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-01-21 11:51:03 +1100

    Update CSS

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -13,7 +13,7 @@
 </main>
 <noscript id="deferred-styles">
   <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:200,500|Roboto:400,700&display=swap" rel="stylesheet"> -->
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
   <?php // if (!is_page_template('page-quiz.php')) : 
   ?>
   <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=2021025" type="text/css" media="all" /> -->
