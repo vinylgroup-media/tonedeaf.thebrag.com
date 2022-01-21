50d24594300106fcb06939acea9f66a8d0d317c2
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-01-21 11:49:57 +1100

    Update footer.php

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -13,11 +13,11 @@
 </main>
 <noscript id="deferred-styles">
   <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:200,500|Roboto:400,700&display=swap" rel="stylesheet"> -->
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;700&display=swap" rel="stylesheet">
   <?php // if (!is_page_template('page-quiz.php')) : 
   ?>
-  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=2021025" type="text/css" media="all" />
-  <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" /> -->
+  <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=2021025" type="text/css" media="all" /> -->
+  <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" />
   <?php // endif; 
   ?>
 </noscript>
