35a2d21533c046f2dacb7ec7f45d5ea5a9828d03
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-01-27 17:30:46 +1100

    Update top menu layout

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -16,8 +16,8 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Poppins:wght@200;300;500&display=swap" rel="stylesheet">
   <?php // if (!is_page_template('page-quiz.php')) : 
   ?>
-  <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20220127" type="text/css" media="all" />
-  <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" /> -->
+  <!-- <link rel="stylesheet" id="tbm-css" href="<?php echo CDN_URL; ?>css/style.css?v=20220127" type="text/css" media="all" /> -->
+  <link rel="stylesheet" id="tbm-css" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo time(); ?>" type="text/css" media="all" />
   <?php // endif; 
   ?>
 </noscript>
