278ae39a84474613e0fd32aecd6b26e3e1295bdb
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-05-12 13:31:54 +1000

    Update tbm-shout-writer-beer.php

--- plugins/tbm-shout-writer-beer/tbm-shout-writer-beer.php
+++ plugins/tbm-shout-writer-beer/tbm-shout-writer-beer.php
@@ -195,8 +195,8 @@ class TBM_Shout_Writer_Beer
   public function shortcode_shout_writer_beer_func($atts)
   {
     wp_enqueue_script($this->plugin_slug, plugins_url('js/scripts.js', __FILE__), array('jquery'), NULL, true);
-    wp_enqueue_style($this->plugin_slug, plugins_url('css/style.min.css', __FILE__));
-    // wp_enqueue_style($this->plugin_slug, plugins_url('css/style.css', __FILE__), time());
+    // wp_enqueue_style($this->plugin_slug, plugins_url('css/style.min.css', __FILE__));
+    wp_enqueue_style($this->plugin_slug, plugins_url('css/style.css', __FILE__), false, '2022-05-12');
     $a = shortcode_atts(array(
       'author' => '',
     ), $atts);
@@ -232,7 +232,7 @@ class TBM_Shout_Writer_Beer
           <a href="https://younghenrys.com/" target="_blank" class="l-logo-beer" rel="noopener" aria-label="Young Henrys">
             <span class="logo-beer" style="display: none;"><img data-src="<?php echo plugins_url('images/yh-logo-blk.png', __FILE__); ?>" class="lazyload"></span>
           </a>
-          <span class="ico-beer"><img src="<?php echo plugin_dir_url(__FILE__); ?>/images/younghenrys.gif" width="35" height="60" loading="lazy"></span>
+          <span class="ico-beer"><img src="<?php echo esc_url(plugins_url('images/younghenrys.gif', __FILE__)); ?>" loading="lazy"></span>
         </div>
         <span class=" text-right">Love this article?<br>Shout <?php echo $a['author']; ?> a beer</span>
       </div>
