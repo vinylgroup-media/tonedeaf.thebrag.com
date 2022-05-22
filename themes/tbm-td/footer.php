f7add62cad7db1067973b51dd64f9f270df1b747
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-05-23 09:52:11 +1000

    Update footer.php

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -83,7 +83,7 @@
       "dateModified": "<?php echo the_modified_date('Y-m-d\TH:i:s+10:00'); ?>",
       "author": {
         "@type": "Person",
-        "name": "<?php echo str_replace(['"', '\\'], ['\"', '\\\\',], $author); ?>"
+        "name": "<?php echo htmlspecialchars($author, ENT_QUOTES); ?>"
       },
       "publisher": {
         "@type": "Organization",
