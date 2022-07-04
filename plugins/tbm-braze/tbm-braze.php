e28858f51585e3421f3b3ab1f55ac773a90b7bb3
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-07-04 10:51:34 +1000

    Update tbm-braze.php

--- plugins/tbm-braze/tbm-braze.php
+++ plugins/tbm-braze/tbm-braze.php
@@ -31,25 +31,6 @@ class Braze
   {
     global $wpdb;
 ?>
-    <!-- Google Tag Manager -->
-    <script>
-      (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-TQC6WRH');
-    </script>
-    <!-- End Google Tag Manager -->
-
     <script type="text/javascript">
       + function(a, p, P, b, y) {
         a.braze = {};
