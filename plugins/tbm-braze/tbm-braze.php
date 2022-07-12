a2430821439a91c7c40732749817eab3e6326bdc
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-07-12 12:00:28 +1000

    move braze function to callable from gam

--- plugins/tbm-braze/tbm-braze.php
+++ plugins/tbm-braze/tbm-braze.php
@@ -24,8 +24,75 @@ class Braze
     $this->plugin_name = 'tbm_braze';
     $this->plugin_slug = 'tbm-braze';
 
-    add_action('wp_head', [$this, 'wp_head']);
+    // add_action('wp_head', [$this, 'wp_head']);
+    add_action('wp_footer', [$this, 'wp_footer']);
+
+    add_action('wp_ajax_get_user_external_id', [$this, 'get_user_external_id']);
+    add_action('wp_ajax_nopriv_get_user_external_id', [$this, 'get_user_external_id']);
   }
+
+  public function get_user_external_id() {
+    global $wpdb;
+		if (is_user_logged_in()) {
+			$user_id = get_current_user_id();
+			if (!$user_id)
+				return;
+			$auth0_user_id = get_user_meta($user_id, $wpdb->prefix . 'auth0_id', true);
+      wp_send_json_success($auth0_user_id);
+      wp_die();
+		} // If user is logged in
+    wp_send_json_error('Not logged in');
+    wp_die();
+  }
+
+  public function wp_footer()
+  {
+?>
+    <script>
+      var user_external_id = null;
+
+      window.callBraze = () => {
+        + function(a, p, P, b, y) {
+          a.braze = {};
+          a.brazeQueue = [];
+          for (var s = "BrazeSdkMetadata DeviceProperties Card Card.prototype.dismissCard Card.prototype.removeAllSubscriptions Card.prototype.removeSubscription Card.prototype.subscribeToClickedEvent Card.prototype.subscribeToDismissedEvent Card.fromContentCardsJson Banner CaptionedImage ClassicCard ControlCard ContentCards ContentCards.prototype.getUnviewedCardCount Feed Feed.prototype.getUnreadCardCount ControlMessage InAppMessage InAppMessage.SlideFrom InAppMessage.ClickAction InAppMessage.DismissType InAppMessage.OpenTarget InAppMessage.ImageStyle InAppMessage.Orientation InAppMessage.TextAlignment InAppMessage.CropType InAppMessage.prototype.closeMessage InAppMessage.prototype.removeAllSubscriptions InAppMessage.prototype.removeSubscription InAppMessage.prototype.subscribeToClickedEvent InAppMessage.prototype.subscribeToDismissedEvent InAppMessage.fromJson FullScreenMessage ModalMessage HtmlMessage SlideUpMessage User User.Genders User.NotificationSubscriptionTypes User.prototype.addAlias User.prototype.addToCustomAttributeArray User.prototype.addToSubscriptionGroup User.prototype.getUserId User.prototype.incrementCustomUserAttribute User.prototype.removeFromCustomAttributeArray User.prototype.removeFromSubscriptionGroup User.prototype.setCountry User.prototype.setCustomLocationAttribute User.prototype.setCustomUserAttribute User.prototype.setDateOfBirth User.prototype.setEmail User.prototype.setEmailNotificationSubscriptionType User.prototype.setFirstName User.prototype.setGender User.prototype.setHomeCity User.prototype.setLanguage User.prototype.setLastKnownLocation User.prototype.setLastName User.prototype.setPhoneNumber User.prototype.setPushNotificationSubscriptionType InAppMessageButton InAppMessageButton.prototype.removeAllSubscriptions InAppMessageButton.prototype.removeSubscription InAppMessageButton.prototype.subscribeToClickedEvent automaticallyShowInAppMessages destroyFeed hideContentCards showContentCards showFeed showInAppMessage toggleContentCards toggleFeed changeUser destroy getDeviceId initialize isPushBlocked isPushPermissionGranted isPushSupported logCardClick logCardDismissal logCardImpressions logContentCardsDisplayed logCustomEvent logFeedDisplayed logInAppMessageButtonClick logInAppMessageClick logInAppMessageHtmlClick logInAppMessageImpression logPurchase openSession requestPushPermission removeAllSubscriptions removeSubscription requestContentCardsRefresh requestFeedRefresh requestImmediateDataFlush enableSDK isDisabled setLogger setSdkAuthenticationSignature addSdkMetadata disableSDK subscribeToContentCardsUpdates subscribeToFeedUpdates subscribeToInAppMessage subscribeToSdkAuthenticationFailures toggleLogging unregisterPush wipeData handleBrazeAction".split(" "), i = 0; i < s.length; i++) {
+            for (var m = s[i], k = a.braze, l = m.split("."), j = 0; j < l.length - 1; j++) k = k[l[j]];
+            k[l[j]] = (new Function("return function " + m.replace(/\./g, "_") + "(){window.brazeQueue.push(arguments); return true}"))()
+          }
+          window.braze.getCachedContentCards = function() {
+            return new window.braze.ContentCards
+          };
+          window.braze.getCachedFeed = function() {
+            return new window.braze.Feed
+          };
+          window.braze.getUser = function() {
+            return new window.braze.User
+          };
+          (y = p.createElement(P)).type = 'text/javascript';
+          y.src = 'https://js.appboycdn.com/web-sdk/4.0/braze.min.js';
+          y.async = 1;
+          (b = p.getElementsByTagName(P)[0]).parentNode.insertBefore(y, b)
+        }(window, document, 'script');
+
+        // initialize the SDK
+        braze.initialize('5fd1c924-ded7-46e7-b75d-1dc4831ecd92', {
+          baseUrl: "sdk.iad-05.braze.com",
+          <?php echo (isset($_ENV) && isset($_ENV['ENVIRONMENT']) && 'sandbox' == $_ENV['ENVIRONMENT']) || ('staging.thebrag.com' == $_SERVER['SERVER_NAME'])
+             ? 'enableLogging: true' : ''; ?>
+        });
+
+        jQuery.get('<?php echo admin_url('admin-ajax.php'); ?>', {
+          action: 'get_user_external_id',
+        }, function (res) {
+          if(res.success && res.data) {
+            user_external_id = res.data;
+            braze.changeUser(user_external_id);
+          }
+        });
+      }
+    </script>
+  <?php
+  } // wp_footer();
 
   public function wp_head()
   {
@@ -36,7 +103,7 @@ class Braze
     if (!is_user_logged_in() && 'AU' != $country_code) {
       return;
     }
-?>
+  ?>
     <script type="text/javascript">
       + function(a, p, P, b, y) {
         a.braze = {};
@@ -81,7 +148,7 @@ class Braze
 <?php
       } // If $auth0_user_id
     } // If user is logged in
-  }
+  } // wp_head()
 }
 
 new Braze();
