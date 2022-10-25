ca12e8193ce7f97cafd01a2ec1954727462b6943
Toby Smith <tobypsmith@gmail.com>
2022-10-25 19:12:17 +1100

    feat: enabling disable ads

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -1965,7 +1965,7 @@ function tbm_set_cookie($data)
 
 function render_ad_tag($tag, $slot_no = 1)
 {
-    if (function_exists('get_field') && get_field('paid_content'))
+    if (function_exists('get_field') && get_field('paid_content', $post->ID)) {
         return;
     if (!file_exists(WP_PLUGIN_DIR . '/tbm-adm/tbm-adm.php'))
         return;
