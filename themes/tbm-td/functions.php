6deaa8e2ef9749920542afbeae03d0f46b66fd1c
Toby Smith <tobypsmith@gmail.com>
2022-12-05 08:01:00 +1100

    fix: disable roy morgan

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -2347,7 +2347,7 @@ add_filter('the_content', function ($content) {
 });
 
 
-add_action('wp_footer', 'inject_roymorgan', 99, 2);
+// add_action('wp_footer', 'inject_roymorgan', 99, 2);
 function inject_roymorgan()
 {
 ?>
