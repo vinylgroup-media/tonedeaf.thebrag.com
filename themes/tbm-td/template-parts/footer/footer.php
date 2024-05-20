a7bd2e9d6b106527830a176e5a5f5df8bc4c7b97
Toby Smith <tobypsmith@gmail.com>
2024-05-20 14:36:47 +1000

    Update footer.php

--- themes/tbm-td/template-parts/footer/footer.php
+++ themes/tbm-td/template-parts/footer/footer.php
@@ -6,9 +6,9 @@
                     <nav>
                         <ul>
                             <li><a href="https://thebrag.media/" target="_blank" rel="noreferrer">Advertise</a></li>
-                            <li><a target="_blank" rel="noopener" href="https://thebrag.media/submit-a-tip/">Submit Tip</a></li>
+                            <li><a target="_blank" rel="noopener" href="https://thebrag.com/media/submit-a-tip/">Submit Tip</a></li>
                             <li><a target="_blank" rel="noopener" href="https://thebrag.media/how-to-submit-an-op-ed-essay/">Submit Op-Ed</a></li>
-                            <li><a target="_blank" rel="noopener" href="https://thebrag.media/submit/">Submit Video</a></li>
+                            <li><a target="_blank" rel="noopener" href="https://thebrag.com/media/submit/">Submit Video</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -20,10 +20,10 @@
         <div class="container footer-menu-2 pb-0">
             <nav class="mx-2">
                 <ul class="d-flex flex-column flex-md-row justify-content-center p-0 mb-0">
-                    <li><a href="https://https://thebrag.com/media/terms-and-conditions/" target="_blank" rel="noreferrer" class="py-1">Competition Ts &amp; Cs</a></li>
+                    <li><a href="https://thebrag.com/media/terms-and-conditions/" target="_blank" rel="noreferrer" class="py-1">Competition Ts &amp; Cs</a></li>
                     <li><a href="https://thebrag.com/media/editorial-code/" target="_blank" rel="noreferrer" class="py-1">Editorial code</a></li>
                     <li><a href="https://thebrag.com/media/terms-of-use/" target="_blank" rel="noreferrer" class="py-1">Terms of use</a></li>
-                    <li><a href="https://https://thebrag.com/media/privacy-policy/" target="_blank" rel="noreferrer" class="py-1">Privacy</a></li>
+                    <li><a href="https://thebrag.com/media/privacy-policy/" target="_blank" rel="noreferrer" class="py-1">Privacy</a></li>
                 </ul>
             </nav>
             <a href="https://vinyl.group/" target="_blank" class="d-flex" style="justify-content: center; align-items: center; padding-top: 1rem; padding-bottom: 1rem;"">
