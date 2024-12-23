1185b5b210e47a9133fa87237739b72726dfcab3
Toby Smith <tobypsmith@gmail.com>
2024-12-24 07:09:46 +1100

    adding cache plugin

---
+++ plugins/tbm-cache/includes/class-tbm-cache.php
@@ -0,0 +1,36 @@
+<?php
+class TBM_Cache {
+
+    private static mixed $instance;
+
+    private function __construct() {}
+
+    public static function get_instance() {
+        if ( !isset( self::$instance ) ) {
+            $c = __CLASS__;
+            self::$instance = new $c();
+        }
+
+        return self::$instance;
+    }
+
+
+    # Initialise
+
+    public function init(): void {
+        add_action( 'edit_post', [ $this, '_delete_cached' ], 100, 2 );
+    }
+
+    public function get_domain(): string {
+        $site_url = site_url();
+
+        return preg_replace("(^https?://)", "", $site_url );
+    }
+
+    public function _delete_cached($post_id, $post): void {
+        $type = $post->post_type == 'post' ? 'posts' : $post->post_type;
+
+       wp_remote_get('http://45.79.238.137:6001/purge/' . $this->get_domain() . '/' . $post_id . '/' . $type);
+       wp_remote_get('http://172.105.183.4:6001/purge/' . $this->get_domain() . '/' . $post_id . '/' . $type );
+    }
+}
