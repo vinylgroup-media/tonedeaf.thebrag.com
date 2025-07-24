bc0d2d4ce2588c2ab56d6e65db387557da5d6649
Talon <talon@offspringdigital.com>
2025-07-24 15:01:41 +0700

    remove RS subscription page

--- themes/tbm-td/functions.php
+++ themes/tbm-td/functions.php
@@ -2592,14 +2592,14 @@ add_filter('the_content', function ($content) {
         $mag_cover = json_decode($mag_cover_res['body']);
     }
 
-    $content .= '<a href="https://au.rollingstone.com/subscribe-magazine/" target="_blank" rel="noopener" class="d-flex flex-column flex-md-row align-items-start rs-subscribe-footer"><div class="d-flex">';
-    if (isset($mag_cover) && '' != $mag_cover) {
-        $content .= '<div class="flex-fill img-wrap"><img src="' . $mag_cover . '" width="100"></div>';
-    }
-    $content .= '<div>Get unlimited access to the coverage that shapes our culture.';
-    $content .= '<div class="d-none d-md-block mt-1"><span class="subscribe">Subscribe</span> to <strong>Rolling Stone magazine</strong></div></div></div>';
-    $content .= '<div class="d-block d-md-none mt-3 w-100"><span class="subscribe">Subscribe</span> to <strong>Rolling Stone magazine</strong></div>';
-    $content .= '</a>';
+//    $content .= '<a href="https://au.rollingstone.com/subscribe-magazine/" target="_blank" rel="noopener" class="d-flex flex-column flex-md-row align-items-start rs-subscribe-footer"><div class="d-flex">';
+//    if (isset($mag_cover) && '' != $mag_cover) {
+//        $content .= '<div class="flex-fill img-wrap"><img src="' . $mag_cover . '" width="100"></div>';
+//    }
+//    $content .= '<div>Get unlimited access to the coverage that shapes our culture.';
+//    $content .= '<div class="d-none d-md-block mt-1"><span class="subscribe">Subscribe</span> to <strong>Rolling Stone magazine</strong></div></div></div>';
+//    $content .= '<div class="d-block d-md-none mt-3 w-100"><span class="subscribe">Subscribe</span> to <strong>Rolling Stone magazine</strong></div>';
+//    $content .= '</a>';
 
     return $content;
 }, 99);
