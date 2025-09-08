1ee1ed7ff9caee81ea8f6e4b721659698ff66610
Demian <demian@offspringdigital.com>
2025-09-08 17:53:36 +0700

    feat: add script footer simlinks

--- themes/tbm-td/footer.php
+++ themes/tbm-td/footer.php
@@ -130,6 +130,9 @@ position: fixed;
 <!-- 22071836792/outofpage/outofpage -->
 <div data-fuse="22779890848"></div>
 
+<!-- Simlinks -->
+<script type="text/javascript" src="https://s.skimresources.com/js/283493X1778391.skimlinks.js"></script>
+
 </body>
 
 </html>
--- themes/tbm-td/header.php
+++ themes/tbm-td/header.php
@@ -108,9 +108,6 @@ $current_url = home_url(add_query_arg([], $GLOBALS['wp']->request));
         })(window, "googletag", "function");
     </script>
 
-    <!-- Simlinks -->
-    <script type="text/javascript" src="https://s.skimresources.com/js/283493X1778391.skimlinks.js"></script>
-
     <style>
         <?php
         echo file_get_contents(get_template_directory() . '/css/reset.css');
