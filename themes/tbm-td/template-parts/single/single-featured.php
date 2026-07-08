6470b55608b5fd15f1ff74524ea38b2520864e2a
khanhntb0209 <kody@offspringdigital.com>
2026-07-08 10:21:55 +0700

    VMO-264 add kwords-exclude class to native articles

--- themes/tbm-td/template-parts/single/single-featured.php
+++ themes/tbm-td/template-parts/single/single-featured.php
@@ -73,42 +73,46 @@ if (!post_password_required($post)) :
             </div>
         </div>
     <?php endif; ?>
-    <article class="single-article featured p-2 p-md-3 pb-1 single-article-<?php echo $count_articles === 1 ? '1' : 'infinite'; ?>" id="<?php the_ID(); ?>">
+    <?php
+    $title = get_post_meta($the_post_id, '_yoast_wpseo_title', true) ? get_post_meta($the_post_id, '_yoast_wpseo_title', true) : get_the_title();
+    if (strpos($title, '%%title%%') !== FALSE) {
+        $title = get_the_title();
+    }
+
+    $tags = get_the_tags($the_post_id);
+    $TagsCD = '';
+    $is_native = false;
+    if ($tags) :
+        foreach ($tags as $tag) :
+            $TagsCD .= $tag->slug . ' ';
+            if ($tag->slug === 'native' || $tag->slug === 'native-content') {
+                $is_native = true;
+            }
+        endforeach; // For Each Tag
+    endif; // If there are tags for the post
+
+    $categories = get_the_category($the_post_id);
+    $CategoryCD = '';
+    if ($categories) :
+        foreach ($categories as $category) :
+            $CategoryCD .= $category->slug . ' ';
+            if ('op-ed-comment' == $category->slug) :
+                $is_oped = true;
+                $oped_termid = $category->term_id;
+            endif;
+        endforeach; // For Each Category
+    endif; // If there are categories for the post
+
+    $genres = get_the_terms($the_post_id, 'genre');
+    $GenreCD = '';
+    if ($genres) :
+        foreach ($genres as $genre) :
+            $GenreCD .= $genre->slug . ' ';
+        endforeach; // For Each Genre
+    endif; // If there are genres for the post
+    ?>
+    <article class="<?php echo $is_native ? 'kwords-exclude ' : ''; ?>single-article featured p-2 p-md-3 pb-1 single-article-<?php echo $count_articles === 1 ? '1' : 'infinite'; ?>" id="<?php the_ID(); ?>">
         <div class="overlay"></div>
-        <?php
-        $title = get_post_meta($the_post_id, '_yoast_wpseo_title', true) ? get_post_meta($the_post_id, '_yoast_wpseo_title', true) : get_the_title();
-        if (strpos($title, '%%title%%') !== FALSE) {
-            $title = get_the_title();
-        }
-
-        $tags = get_the_tags($the_post_id);
-        $TagsCD = '';
-        if ($tags) :
-            foreach ($tags as $tag) :
-                $TagsCD .= $tag->slug . ' ';
-            endforeach; // For Each Tag
-        endif; // If there are tags for the post
-
-        $categories = get_the_category($the_post_id);
-        $CategoryCD = '';
-        if ($categories) :
-            foreach ($categories as $category) :
-                $CategoryCD .= $category->slug . ' ';
-                if ('op-ed-comment' == $category->slug) :
-                    $is_oped = true;
-                    $oped_termid = $category->term_id;
-                endif;
-            endforeach; // For Each Category
-        endif; // If there are categories for the post
-
-        $genres = get_the_terms($the_post_id, 'genre');
-        $GenreCD = '';
-        if ($genres) :
-            foreach ($genres as $genre) :
-                $GenreCD .= $genre->slug . ' ';
-            endforeach; // For Each Genre
-        endif; // If there are genres for the post
-        ?>
         <div class="cats mb-3 text-center" data-category="<?php echo $CategoryCD; ?>" data-tags="<?php echo $TagsCD; ?>" data-genre="<?php echo $GenreCD; ?>">
             <?php
             if ($genres) :
